2462e0f7ce037a1a1ce942fdd4989c26398a7270
AGNES SANTOS <agnes.nuguid@uap.asia>
2023-02-18 00:37:19 +0800

    Initial Solution

--- src/composables/useToast.ts
+++ src/composables/useToast.ts
@@ -1,3 +1,31 @@
+
+import { ref } from 'vue'
+export interface Toast {
+    type: 'Info' | 'Success' | 'Error',
+    message: string,
+    duration: number,
+    id?: number
+}
+
+const toasts = ref<Toast[]>([]);
+
 export function useToast() {
+    let id: number = 0;
     
+    // create toast
+    function createToast(toast: Toast) {
+        const _toast: Toast = {...toast, id: id++}
+        toasts.value.push(_toast)
+        setTimeout(() => destroyToast(_toast), _toast.duration)
+    }
+
+    // destroy toasts
+    function destroyToast(toast: Toast) {
+        toasts.value.splice(toasts.value.indexOf(toast), 1)
+    }
+
+    return {
+        toasts,
+        createToast
+    }
 }
---
+++ src/constants/injectionKeys.ts
@@ -0,0 +1,3 @@
+import { InjectionKey } from 'vue'
+
+export const ToastCreateKey: InjectionKey<Function> = Symbol('createToast')
